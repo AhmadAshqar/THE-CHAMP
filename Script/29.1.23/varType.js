faa0a55bb863691e86e700edff36b46354f5e2b3
AhmadAshqar <wowking54@gmail.com>
2023-01-29 10:07:08 +0200

    ss

---
+++ Script/29.1.23/varType.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = "20";
+var c = a + b;
+var e;
+var d = true;
+
+console.log("a is " + typeof a)
+console.log("a is " + typeof b)
+console.log("c = " + c + " and it is " + typeof c)
+console.log("e is " + typeof e)
+console.log("d is " + typeof d)
